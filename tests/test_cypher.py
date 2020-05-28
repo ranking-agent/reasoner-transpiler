b2636f778fc7586956337df6694145fb304acfec
Max Wang <max@covar.com>
2020-05-28 16:50:33 -0400

    adding dummy neo4j setup

--- tests/test_cypher.py
+++ tests/test_cypher.py
@@ -1,57 +1,50 @@
 """Test Reasoner->Cypher transpiler."""
 import pytest
+from neo4j import GraphDatabase
 
 from reasoner.cypher import get_query, get_match_clause
 
-
-def test_match():
-    """Test MATCH clause generation."""
-    qgraph = {
-        "nodes": [
-            {
-                "id": "n0",
-                "curie": [
-                    "MONDO:0100096",
-                    "DOID:4325",
-                ],
-                "type": "disease",
-            },
-            {
-                "id": "n1",
-                "curie": {
-                    "MONDO:0100096",
-                    "DOID:4325",
-                },
-                "type": "disease",
-            },
-        ],
-        "edges": [],
-    }
-    clause = get_match_clause(qgraph)
-    print(clause)
+uri = 'bolt://localhost:7687'
+driver = GraphDatabase.driver(uri, auth=None)
 
 
-def test_query():
-    """Test full query generation."""
+with driver.session() as session:
+    session.run("MATCH (m) DETACH DELETE m")
+    session.run("LOAD CSV WITH HEADERS FROM 'file:///total_nodes.csv' "
+                "AS row "
+                "CALL apoc.create.node([row.label], {"
+                "name: row.name, id: row.name,"
+                "subtype: row.subtype, gender: row.gender,"
+                "occurences: toInteger(row.occurences)"
+                "}) YIELD node "
+                "RETURN count(*)")
+    session.run("LOAD CSV WITH HEADERS FROM 'file:///triples.csv' "
+                "AS edge "
+                "MATCH (source), (target) "
+                "WHERE source.name = edge.source_id AND target.name = edge.target_id "
+                "CALL apoc.create.relationship(source, toUpper(edge.predicate), {predicate: edge.predicate}, target) YIELD rel "
+                "RETURN count(*)")
+    print('Everything loaded')
     qgraph = {
         "nodes": [
             {
                 "id": "n1",
-                "type": "gene",
+                "type": "Weapon",
             },
             {
                 "id": "n0",
-                "curie": "MONDO:0100096",
-                "type": "disease",
-                "communicable": True,
-                "US deaths": 75000,
+                "curie": "Frodo",
+                "type": "Person",
+                "gender": "male",
+                "occurences": 2040,
             },
             {
                 "id": "n2",
+                "type": "Person",
                 "curie": [
-                    "MONDO:0004979",
+                    "Bilbo",
                 ],
-            }
+            },
         ],
         "edges": [
             {
@@ -63,15 +56,94 @@ def test_query():
                 "id": "e01",
                 "source_id": "n0",
                 "target_id": "n1",
-                "type": "related_to",
+                "type": "INHERITS",
             },
             {
                 "id": "e21",
                 "source_id": "n2",
                 "target_id": "n1",
                 "type": [
-                    "related_to",
-                    "associated_with",
+                    "WIELDS",
+                    "FINDS",
+                ],
+            }
+        ],
+    }
+    records = session.run(get_query(qgraph))
+    for record in records:
+        print(record)
+
+
+def test_match():
+    """Test MATCH clause generation."""
+    qgraph = {
+        "nodes": [
+            {
+                "id": "n0",
+                "curie": [
+                    "Frodo",
+                    "Sam",
+                ],
+                "type": "Person",
+            },
+            {
+                "id": "n1",
+                "curie": {
+                    "Frodo",
+                    "Sam",
+                },
+                "type": "Person",
+            },
+        ],
+        "edges": [],
+    }
+    clause = get_match_clause(qgraph)
+    print(clause)
+    # assert get_match_clause(qgraph) == "MATCH (`n0`:`Person` {}) USING INDEX `n0`:Person(id) WHERE `n0`.id = \"Frodo\" OR `n0`.id = \"Sam\" MATCH (`n1`:`Person` {`id`: \"{'Frodo', 'Sam'}\"}) USING INDEX `n1`:Person(id)"
+
+
+def test_query():
+    """Test full query generation."""
+    qgraph = {
+        "nodes": [
+            {
+                "id": "n1",
+                "type": "Weapon",
+            },
+            {
+                "id": "n0",
+                "curie": "Frodo",
+                "type": "Person",
+                "gender": "male",
+                "occurences": 2040,
+            },
+            {
+                "id": "n2",
+                "type": "Person",
+                "curie": [
+                    "Bilbo",
+                ],
+            },
+        ],
+        "edges": [
+            {
+                "id": "e02",
+                "source_id": "n0",
+                "target_id": "n2",
+            },
+            {
+                "id": "e01",
+                "source_id": "n0",
+                "target_id": "n1",
+                "type": "INHERITS",
+            },
+            {
+                "id": "e21",
+                "source_id": "n2",
+                "target_id": "n1",
+                "type": [
+                    "WIELDS",
+                    "FINDS",
                 ],
             }
         ],
