391a7be06391cefc2db53b54820d2f9fba089dac
Patrick Wang <patrick@covar.com>
2020-12-16 16:05:52 -0500

    Add properties to test KG

--- tests/initialize_db.py
+++ tests/initialize_db.py
@@ -42,16 +42,17 @@ def main():
         session.run("MATCH (m) DETACH DELETE m")
         session.run("LOAD CSV WITH HEADERS FROM 'file:///nodes.csv' "
                     "AS row "
-                    "CALL apoc.create.node([row.category], {"
+                    "CALL apoc.create.node([row.category], apoc.map.merge({"
                     "name: row.name, id: row.id"
-                    "}) YIELD node "
+                    "}, apoc.convert.fromJsonMap(row.props))) YIELD node "
                     "RETURN count(*)")
         session.run("LOAD CSV WITH HEADERS FROM 'file:///edges.csv' "
                     "AS edge "
                     "MATCH (subject), (object) "
                     "WHERE subject.id = edge.subject AND object.id = edge.object "
                     "CALL apoc.create.relationship(subject, edge.predicate, "
-                    "{predicate: edge.predicate, id: edge.id}, object) YIELD rel "
+                    "apoc.map.merge({predicate: edge.predicate, id: edge.id}, "
+                    "apoc.convert.fromJsonMap(edge.props)), object) YIELD rel "
                     "RETURN count(*)")
     LOGGER.info('Done. Neo4j is ready for testing.')
 
