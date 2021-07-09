bd0631430e4200d4639e97ee088a400109423bde
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-07-08 20:10:04 -0400

    Bump to a more confidence-inducing version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="reasoner",
-    version="0.1.0-dev",
+    version="1.7.1",
     author="Patrick Wang",
     author_email="patrick@covar.com",
     url="https://github.com/ranking-agent/reasoner",
