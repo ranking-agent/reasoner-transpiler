cd1925f4503e1b094f9f8c6205025778e117839b
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-08-04 09:28:05 -0400

    Change package name

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-"""Setup file for reasoner package."""
+"""Setup reasoner-transpiler package."""
 from setuptools import setup
 
 setup(
-    name="reasoner",
+    name="reasoner-transpiler",
     version="1.7.1",
     author="Patrick Wang",
     author_email="patrick@covar.com",
     url="https://github.com/ranking-agent/reasoner-transpiler",
     description="TRAPI → Cypher transpiler",
-    packages=["reasoner"],
-    package_data={"reasoner": ["attribute_types.json"]},
+    packages=["reasoner_transpiler"],
+    package_data={"reasoner_transpiler": ["attribute_types.json"]},
     zip_safe=False,
     license="MIT",
     python_requires=">=3.6",
