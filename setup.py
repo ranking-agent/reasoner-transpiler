5e072473479eb61faa113ab6d7e12a6979d853f8
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-09-09 15:10:24 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="reasoner-transpiler",
-    version="1.8.1",
+    version="1.9.0",
     author="Patrick Wang",
     author_email="patrick@covar.com",
     url="https://github.com/ranking-agent/reasoner-transpiler",
