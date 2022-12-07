433f9cfa1f21ec9aa9ab4762c36c9218a1034ea6
YaphetKG <45075777+YaphetKG@users.noreply.github.com>
2022-12-07 09:17:28 -0500

    bumps version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="reasoner-transpiler",
-    version="1.11.8",
+    version="1.11.9",
     author="Patrick Wang",
     author_email="patrick@covar.com",
     url="https://github.com/ranking-agent/reasoner-transpiler",
