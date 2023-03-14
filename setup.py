cfc49577c5af3bb54e5ab64a4ff1c4e27793545e
YaphetKG <45075777+YaphetKG@users.noreply.github.com>
2023-03-14 12:08:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="reasoner-transpiler",
-    version="1.11.10",
+    version="1.11.11",
     author="Patrick Wang",
     author_email="patrick@covar.com",
     url="https://github.com/ranking-agent/reasoner-transpiler",
