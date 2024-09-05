689b8d95e1a1f25f1b878c0a43bc65df66191cf3
Evan Morris <evandietzmorris@gmail.com>
2024-09-05 02:36:44 -0400

    bumping set up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="reasoner-transpiler",
-    version="2.2.5",
+    version="2.2.6",
     author="Patrick Wang",
     author_email="patrick@covar.com",
     maintainer="Evan Morris",
