289f467673976fd4feca80e2c170bbd2d9413ecc
Evan Morris <evandietzmorris@gmail.com>
2025-04-30 16:31:30 -0400

    cleaning up the usage of biolink for attribute type mapping

--- reasoner_transpiler/biolink.py
+++ reasoner_transpiler/biolink.py
@@ -1,6 +1,8 @@
 import os
 from bmt import Toolkit
 
+from bmt.toolkit import ClassDefinition
+from bmt.toolkit import SlotDefinition
 
 BIOLINK_MODEL_VERSION = os.environ.get('BL_VERSION', '4.2.6-rc5')
 BIOLINK_MODEL_SCHEMA_URL = f"https://raw.githubusercontent.com/biolink/biolink-model/v{BIOLINK_MODEL_VERSION}/biolink-model.yaml"
@@ -8,3 +10,19 @@ PREDICATE_MAP_URL = f"https://raw.githubusercontent.com/biolink/biolink-model/v{
 
 bmt = Toolkit(schema=BIOLINK_MODEL_SCHEMA_URL, predicate_map=PREDICATE_MAP_URL)
 ALL_BIOLINK_ENUMS = bmt.view.all_enums().keys()
+
+
+def is_biolink_slot(obj):
+    return isinstance(obj, SlotDefinition)
+
+
+def get_slot_uri(obj):
+    return obj["slot_uri"]
+
+
+def is_biolink_class(obj):
+    return isinstance(obj, ClassDefinition)
+
+
+def get_class_uri(obj):
+    return obj["class_uri"]
