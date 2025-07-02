01ac6c6064b84730fa74f19e8fc70f5eeadf8a36
Chris Bizon <bizon@renci.org>
2025-07-02 11:03:17 -0400

    edges

--- tests/memgraph_csv/create_memgraph_csv.py
+++ tests/memgraph_csv/create_memgraph_csv.py
@@ -20,6 +20,31 @@ with open("tests/neo4j_csv/nodes.csv", "r") as infile, open("tests/memgraph_csv/
         else:
             outfile.write(line)
 
+# we're going to have a lot of edges with null props this way, which isn't what most graphs will look like but it shouldn't affect the tests
 with open("tests/neo4j_csv/edges.csv", "r") as infile, open("tests/memgraph_csv/edges.csv", "w") as outfile:
+    header = infile.readline().split(',')[:-1]
+    newprops = ["primary_knowledge_source", "fda_approved", "aggregator_knowledge_source",
+               "publications", "p_value", "non_biolink_attribute", "attributes",
+               "qualified_predicate", "object_aspect_qualiier", "object_direction_qualifier",
+               "bogus_knowledge_source"]
+    header += newprops
+    header = ','.join(header) + '\n'  # remove the last comma and add newline
+    outfile.write(header)
     for line in infile:
-        outfile.write(line)
+        parts = line.strip().split(',')
+        props = ','.join(parts[4:])[1:-1] \
+            .replace('\\\\\\"', "'") \
+            .replace('\\', "") \
+            .replace("false","False") \
+            .replace("true", "True")
+        if len(props) == 0:
+            props_dict = {}
+        else:
+            props_dict = eval(props)
+        outparts = parts[:4]
+        for prop in newprops:
+            if prop in props_dict:
+                outparts.append(str(props_dict[prop]))
+            else:
+                outparts.append('')
+        outfile.write(",".join(outparts) + '\n')
